7ae4dda5d940425b4f842a339170e5ab120c7b5b
Mateo Cimirro <mateocimirro81@gmail.com>
2024-07-01 19:05:57 +0200

    added hashed passwords

--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,30 @@
 import itertools
 import string
 import time
+import hashlib
+
+def hashed():
+    global hashedornot, target_variable, hashed_target
+    hashedornot = input('Is your password hashed? (y/n): ').lower()
+    if hashedornot == "y":
+        target_variable = input("Enter the unhashed password to find the hash: ")
+        hashed_target = target_variable
+    else:
+        target_variable = input("Enter the password: ")
+    return hashedornot
+
+def find_matching_combination_hashed(target_hash):
+    ascii_characters = string.printable  # This includes all ASCII printable characters
+    combinations_tried = 0
+    
+    length = 1
+    while True:
+        for combination in itertools.product(ascii_characters, repeat=length):
+            combination_str = ''.join(combination)
+            combinations_tried += 1
+            if hashlib.md5(combination_str.encode()).hexdigest() == target_hash:
+                return combination_str, combinations_tried
+        length += 1
 
 def find_matching_combination(target):
     ascii_characters = string.printable  # This includes all ASCII printable characters
@@ -15,12 +39,14 @@ def find_matching_combination(target):
                 return combination_str, combinations_tried
         length += 1
 
-# Main script
-target_variable = input("Enter the password to find: ")
+hashed()
 
 start_time = time.time()
 
-matching_combination, combinations_tried = find_matching_combination(target_variable)
+if hashedornot == "y":
+    matching_combination, combinations_tried = find_matching_combination_hashed(hashed_target)
+else:
+    matching_combination, combinations_tried = find_matching_combination(target_variable)
 
 end_time = time.time()
 
